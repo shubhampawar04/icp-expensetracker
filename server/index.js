ab8262541c73068fc93f30e9524d54623acda107
shubhampawar04 <pawarshubham81668@gmail.com>
2024-09-02 12:49:51 +0530

    Initial Setup

---
+++ server/index.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = express ();
+app.use(express.json());
+app.use(cors());
+
+
+// connect to mangoDB
+
+const connectDB = async () => {
+    const conn = await mongoose.connect(process.env.MANGO_URI,)
+    if (conn) {
+        console.log(`MongoDB connected successfully..!✅`);
+    }
+};
+connectDB();
+
+app.get('/', (req, res) => {
+    res.json({
+        message:`Welcome to Expense Tracker API`
+    })
+})
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
